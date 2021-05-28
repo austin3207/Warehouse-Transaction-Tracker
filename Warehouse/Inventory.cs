7349c4474f5fc56ab31b862aaee512eb9f95f97e
austin3207 <38427654+austin3207@users.noreply.github.com>
2021-05-28 14:22:37 -0400

    Add files via upload

---
+++ Warehouse/Inventory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warehouse
+{
+    class Inventory
+    {
+        String location;
+        public int part102 = 0;
+        public int part215 = 0;
+        public int part410 = 0;
+        public int part525 = 0;
+        public int part711 = 0;
+
+        public Inventory(String loc, int p1, int p2, int p4, int p5, int p7) //contructor 
+        {
+            location = loc;
+            part102 = p1;
+            part215 = p2;
+            part410 = p4;
+            part525 = p5;
+            part711 = p7;
+        }
+        public void Display()   //Simple Display for location and number of each part
+        {
+            Console.WriteLine(location);
+            Console.WriteLine(part102 + " " + part215 + " " + part410 + " " + part525 + " " + part711);
+        }
+        public int getPart(int partNum)//Used to compare number of a specific part
+        {                              //a warehouse has in stock to determine buyers and sellers
+            int num = 0;
+            switch (partNum)
+            {
+                case 102:
+                    num = part102;
+                    break;
+                case 215:
+                    num = part215;
+                    break;
+                case 410:
+                    num = part410;
+                    break;
+                case 525:
+                    num = part525;
+                    break;
+                case 711:
+                    num = part711;
+                    break;
+                default:
+                    break;
+            }
+            return num;
+        }
+        public void Transaction(String a, int part, int numParts)
+        {
+            if (a.Equals('S'))  //Determines if the parts will be added or subtracted from the warehouse(P/S)
+            {
+                numParts = -numParts;
+            }
+            switch (part)       //Chooses part specified by the transaction
+            {
+                case 102:
+                    part102 += numParts;
+                    break;
+                case 215:
+                    part215 += numParts;
+                    break;
+                case 410:
+                    part410 += numParts;
+                    break;
+                case 525:
+                    part525 += numParts;
+                    break;
+                case 711:
+                    part711 += numParts;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+    }
+    
+}
---
+++ Warehouse/WareHouses.cs
@@ -0,0 +1,91 @@
+﻿using System;
+/*
+ * Austin Rogers
+ * 3/5/2021
+ * Warehouse Project
+ * 
+ */
+namespace Warehouse
+{
+    class WareHouses
+    {
+        static void Main(string[] args)
+        {
+               /*
+                * Read inventory file to setup Warehouse objects
+                * 
+                * May need to modify file path to run on other machines
+                */
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"C:\Users\Austin\source\repos\Warehouse\Inventory.txt");
+            String line;
+            String[] word;
+            String[] locations = { "Atlanta", "Baltimore", "Chicago", "Denver", "Ely", "Fargo" };
+            Inventory[] arr = new Inventory[6];
+            for(int i = 0; i < 6; i++)
+            {
+                line = file.ReadLine();
+                word = line.Split(", ");
+                arr[i] = new Inventory(locations[i], Int32.Parse(word[0]), Int32.Parse(word[1]), Int32.Parse(word[2]), Int32.Parse(word[3]), Int32.Parse(word[4]));
+            }
+            
+            //Displays Initial State of each Warehouse
+            Console.WriteLine("Initial State of Warehouses:");
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i].Display();
+            }
+            Console.WriteLine("\n");
+
+            /*
+             * Beginning of Transactions
+             */
+            String[] log = System.IO.File.ReadAllLines(@"C:\Users\Austin\source\repos\Warehouse\Transactions.txt");
+            int count = 0;
+            int part;
+            for(int i = 0; i < log.Length; i++)
+            {
+                word = log[i].Split(", ");
+
+                //determines which warehouse to buy or sell from.
+                if (word[0].Equals("S")) //for sales transactions
+                {
+                    count = 0;
+                    part = Int32.Parse(word[1]);
+                    int numParts = Int32.Parse(word[2]);
+                    for(int j = 1; j < 6; j++)
+                    {
+                        if (arr[count].getPart(part) < arr[j].getPart(part))
+                        {
+                            count = j;
+                        }
+                    }
+                    arr[count].Transaction(word[0], part, numParts);
+                    Console.WriteLine(locations[count] + " has made a transaction.");
+                }
+                else        //for purchases
+                {
+                    count = 0;
+                    part = Int32.Parse(word[1]);
+                    int numParts = Int32.Parse(word[2]);
+                    for (int j = 1; j < 6; j++)
+                    {
+                        if (arr[count].getPart(part) > arr[j].getPart(part))
+                        {
+                            count = j;
+                        }
+                    }
+                    arr[count].Transaction(word[0], part, numParts);
+                    Console.WriteLine(locations[count] + " has made a transaction.");
+                }
+            }
+            //Final Display
+            Console.WriteLine("\n");
+            Console.WriteLine("End of day:");
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i].Display();
+            }
+        }
+    }
+}
